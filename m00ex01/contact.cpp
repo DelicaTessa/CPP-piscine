0fc4169e47e0f86544b32dd02120875b525bfe99
Tessa Clement <tclement@student.codam.nl>
2020-07-29 20:39:38 +0200

    modified ex01

--- m00ex01/contact.cpp
+++ m00ex01/contact.cpp
@@ -6,59 +6,69 @@
 /*   By: tclement <tclement@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/28 18:46:42 by tclement      #+#    #+#                 */
-/*   Updated: 2020/07/28 19:03:41 by tclement      ########   odam.nl         */
+/*   Updated: 2020/07/29 20:26:41 by tclement      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "phonebook.hpp"
+#include "contact.hpp"
 
-void  ft_basic(void)
+contact::contact (void)
 {
-    std::string command;
-    int i;
-    i = 0;
-    std::cout << "What is your command?";
-    std::cin >> command;
-    if (command == "ADD")
-    {
-        while (i < 8)
-        {
-            contact[i].ft_add();
-            i++;
-            ft_basic();
-        }
-        if (i >= 8)
-        {
-            std::cout << "Unfortunately the phonebook is full" << std::endl;
-            ft_basic();
-        }
-    }
-    else if (command == "EXIT")
-        exit(1);
-    else if (command == "SEARCH")
-    {
-        std::cout << "under construction";
-        ft_basic();
-    }
-    else
-    {
-        std::cout << "Invalid input. Please try again." << std::endl;
-        ft_basic();
-    }
+    return;
 }
 
-void    contact::ft_add()
+contact::~contact (void)
+{
+    return;
+}
+
+void    contact::add()
 {
         std::cout << "What is your first name?" << std::endl;
-        std::cin >> input[0];
+        std::cin >> _firstname;
         std::cout << "What is your last name?" << std::endl;
-        std::cin >> input[1];
         std::cout << "What is your nickname?" << std::endl;
-        std::cin >> input[2];
+        
 }
+
 
 int main(void)
 {
-    ft_basic();
+    int i;
+    i = 0;
+    
+    std::string command;
+    contact phonebook[8];
+    std::cout << "What is your command?";
+    std::cin >> command;
+    while(command != "EXIT")
+    {
+        if (command == "ADD")
+        {
+            if (i == 8)
+            {
+                std::cout << "Unfortunately the phonebook is full" << std::endl;
+            }
+            if(i < 8 )
+            {
+               phonebook[i].add();
+                i++;
+            }
+         }
+        else if (command == "EXIT")
+            exit(1);
+        else if (command == "SEARCH")
+        {
+            std::cout << "under construction" << std::endl;
+        }
+        else
+        {
+            std::cout << "Invalid input. Please try again." << std::endl;
+        }
+        std::cout << "What is your command?";
+        std::cin >> command;
+    }
+    if (command == "EXIT")
+        exit(1);
     return (0);
 }
--- m00ex01/contact.hpp
+++ m00ex01/contact.hpp
@@ -1,34 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   phonebook.hpp                                      :+:    :+:            */
+/*   contact.hpp                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: tclement <tclement@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/27 18:35:30 by tclement      #+#    #+#                 */
-/*   Updated: 2020/07/28 19:01:27 by tclement      ########   odam.nl         */
+/*   Updated: 2020/07/29 20:03:24 by tclement      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
 
 #include <iostream>
 
 
 class contact{
-public:
-   contact(void);
-   ~contact(void);
+   public:
+      contact();
+     ~contact();
 
 
-void  ft_add(void);
-private:
- std::string input[11];
+   void  add();
+   void menu(void);
+   private:
+    std::string _firstname;
+    std::string _lastname;
+    std::string _nickname;
+    std::string _login;
+    std::string _address;
+    std::string _email;
+    std::string _meal;
+    std::string _underwear;
+    std::string _secret;
 };
 
-void  ft_basic(void);
-
-
-
+void test(void);
 #endif
